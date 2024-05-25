88f59a01f5c2af832dd7de20c50e7af5bcded1fd
khaser <a-horohorin@mail.ru>
2024-05-25 15:12:22 +0300

    client: deregister host on server

--- client/client.py
+++ client/client.py
@@ -42,7 +42,7 @@ action_group.add_argument('--sign-up', '-s', dest='handle', help="sign-up as adm
 action_group.add_argument('--register-project', '-r', dest='proj_name', help="register new service with `proj-name`")
 action_group.add_argument('--delete-project', action='store_true', help="remove service")
 
-def spawn_tunnel(local_port, remote_port):
+def spawn_tunnel(local_port, remote_port, host_token):
 
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.client.AutoAddPolicy)
@@ -57,7 +57,7 @@ def spawn_tunnel(local_port, remote_port):
         print("Connect request registred")
         sock = try_connect("127.0.0.1", local_port)
         if sock is None:
-            deregister_hosting()
+            deregister_hosting(host_token)
             return
         thr = threading.Thread(
             target=resend_routine, args=(chan, sock)
@@ -95,8 +95,14 @@ def resend_routine(chan, sock, checkOnly=False):
     print("Tunnel closed from %r" % (chan.origin_addr,))
 
 
-def deregister_hosting():
-    print("TODO: deregister")
+def deregister_hosting(host_token):
+    r = requests.delete(f'{REST_URL}/service/hosts', json={'host_token': host_token})
+    if r.status_code == 200:
+        print("Host successfully deregistered")
+    elif r.status_code == 403:
+        print("Incorrect host token, client-server protocol BUG!")
+    else:
+        print("Internal server error")
 
 
 def main():
@@ -111,85 +117,85 @@ def main():
 
     print(args)
 
-    if args.connect:
-        pass
-    elif args.disconnect:
-        pass
-    elif args.start_hosting:
-        if not args.token:
-            print("Specify you admin token to be able manage projects")
-            exit (1)
-        if not args.port:
-            print("For `--start-hosting` you must specify `--port` for sharing")
-            exit (1)
-        r = requests.post(f'{REST_URL}/service/hosts', json={'service_token': args.token})
-        if r.status_code == 200:
-            remote_port, host_token = r.json().values()
-            remote_port = int(remote_port)
+    try:
+        if args.connect:
+            pass
+        elif args.disconnect:
+            pass
+        elif args.start_hosting:
+            if not args.token:
+                print("Specify you admin token to be able manage projects")
+                exit (1)
+            if not args.port:
+                print("For `--start-hosting` you must specify `--port` for sharing")
+                exit (1)
+            r = requests.post(f'{REST_URL}/service/hosts', json={'service_token': args.token})
+            if r.status_code == 200:
+                remote_port, host_token = r.json().values()
+                remote_port = int(remote_port)
 
-            sock = try_connect("127.0.0.1", args.port)
-            if not sock:
-                print(f"Failed to connect to local port, check that you provide application on port {args.port}")
-                deregister_hosting()
+                sock = try_connect("127.0.0.1", args.port)
+                if not sock:
+                    print(f"Failed to connect to local port, check that you provide application on port {args.port}")
+                    deregister_hosting(host_token)
+                    exit(1)
+                else:
+                    sock.close()
+
+                print("Hosting will be stopped automatically when specified service port is closed")
+
+                print(f"DEBUG: host_token {host_token}, local_port {args.port}, remote_port {remote_port}")
+                if args.detach:
+                    with daemon.DaemonContext():
+                        spawn_tunnel(args.port, remote_port, host_token)
+                else:
+                    spawn_tunnel(args.port, remote_port, host_token)
+                exit(0)
+            elif r.status_code == 409:
+                print(f"User with handle `{args.handle}` alredy exists. Please, try different handle")
                 exit(1)
             else:
-                sock.close()
-
-            print_with_hint(host_token, "Hosting will be stopped automatically when specified service port is closed")
-
-            print(f"DEBUG: local_port {args.port}, remote_port {remote_port}")
-            if args.detach:
-                with daemon.DaemonContext():
-                    spawn_tunnel(args.port, remote_port)
+                print("Unrecognized server error")
+                exit(2)
+        elif args.handle:
+            r = requests.get(f'{REST_URL}/admin/register', params={'handle': args.handle})
+            if r.status_code == 200:
+                print_with_hint(r.text,
+                                "Above you can see you administrator token. Save it and specify\n"
+                                "for project-managing operations (e.g. --register-project) using --token argument"
+                                )
+                exit(0)
+            elif r.status_code == 409:
+                print(f"User with handle `{args.handle}` alredy exists. Please, try different handle")
+                exit(1)
             else:
-                spawn_tunnel(args.port, remote_port)
-            exit(0)
-        elif r.status_code == 409:
-            print(f"User with handle `{args.handle}` alredy exists. Please, try different handle")
-            exit(1)
-        else:
-            print("Unrecognized server error")
-            exit(2)
-    elif args.stop_hosting:
-        pass
-    elif args.handle:
-        r = requests.get(f'{REST_URL}/admin/register', params={'handle': args.handle})
-        if r.status_code == 200:
-            print_with_hint(r.text,
-                            "Above you can see you administrator token. Save it and specify\n"
-                            "for project-managing operations (e.g. --register-project) using --token argument"
-                            )
-            exit(0)
-        elif r.status_code == 409:
-            print(f"User with handle `{args.handle}` alredy exists. Please, try different handle")
-            exit(1)
-        else:
-            print("Unrecognized server error")
-            exit(2)
-    elif args.proj_name:
-        if not args.token:
-            print("Specify you admin token to be able manage projects")
-            exit (1)
-        r = requests.post(f'{REST_URL}/service/register', json={'admin_token': args.token, 'name': args.proj_name})
-        if r.status_code == 200:
-            print_with_hint(r.text,
-                            "Above you can see service hoster token. Save it and share with peoples, who\n"
-                            "should be able to host your service with `--start-hosting`. \n"
-                            "Of course, you can use that token by yourself"
-                            )
-            exit(0)
-        elif r.status_code == 403:
-            print("Incorrect credentials")
-            exit(1)
-        elif r.status_code == 409:
-            print(f"Your project `{args.proj_name}` alredy exists. Please, pick different name, or remove project with `--delete-project`")
-            exit(1)
-        else:
-            print("Unrecognized server error")
-            exit(2)
-    elif args.delete_project:
-        pass
-
+                print("Unrecognized server error")
+                exit(2)
+        elif args.proj_name:
+            if not args.token:
+                print("Specify you admin token to be able manage projects")
+                exit (1)
+            r = requests.post(f'{REST_URL}/service/register', json={'admin_token': args.token, 'name': args.proj_name})
+            if r.status_code == 200:
+                print_with_hint(r.text,
+                                "Above you can see service hoster token. Save it and share with peoples, who\n"
+                                "should be able to host your service with `--start-hosting`. \n"
+                                "Of course, you can use that token by yourself"
+                                )
+                exit(0)
+            elif r.status_code == 403:
+                print("Incorrect credentials")
+                exit(1)
+            elif r.status_code == 409:
+                print(f"Your project `{args.proj_name}` alredy exists. Please, pick different name, or remove project with `--delete-project`")
+                exit(1)
+            else:
+                print("Unrecognized server error")
+                exit(2)
+        elif args.delete_project:
+            pass
+    except ConnectionRefusedError as e:
+        print("Failed connect to kdispatch proxy server: {HOST_IP}")
 
 
 if __name__ == "__main__":
